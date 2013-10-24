7dc36a12ae6399aff559a8cfc1657affa2712614
Jozsef Nyitrai <nyitrai.jozsef@gmail.com>
2013-10-24 16:24:26 +0200

    code cleaning

--- html.js
+++ html.js
@@ -1,12 +1,10 @@
 var monitor = require('./index');
-var test = monitor.test
-var call_callback = monitor.call_callback
 
-exports.html_title = 'Website Monitor'
+exports.html_title = 'Website Monitor';
 var html_header = '<html><head><title>Website Monitor</title>';
 var html_header = '<meta http-equiv="refresh" content="60">';
 html_header += '<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">';
-d = new Date();
+var d = new Date();
 html_header += '<body><h1>Status</h1>';
 html_header += '<div class="text-muted">' + d.toLocaleDateString() + ' ' + d.toLocaleTimeString() + '</div>';
 
@@ -23,15 +21,15 @@ exports.html = function(url, callback) {
     table_row += '</tr>';
     buffered_log(table_row);
     monitor.call_callback(http_status, callback);
-  };
+  }
 
   var log_buffer = [];
   function buffered_log() {
     log_buffer.push([].slice.call(arguments));
-  };
+  }
 
   buffered_log(exports.html_header);
-  test(url, function(http_status) { html_output(http_status, callback); })
+  monitor.test(url, function(http_status) { html_output(http_status, callback); });
   process.on('exit', function() {
     for(var j = 0; j < log_buffer.length; j++) {
       process.stdout.write(log_buffer[j] + "\n");
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var request = require('request');
 
 function status_test(url, callback) {
-  request(url, function(error, response, body) {
+  request(url, function(error, response) {
     var message = 'SUCCESS';
     var code = 0;
     var url_without_passwd = url.replace(/(https?:\/\/)(.*:.*@)?(.*)/, '$1$3');
@@ -15,52 +15,51 @@ function status_test(url, callback) {
     }
 
     callback({
-      "success": (message == 'SUCCESS'),
-      "error": (message == 'ERROR'),
+      "success": (message === 'SUCCESS'),
+      "error": (message === 'ERROR'),
       "message": message,
       "error_message": error,
       "url": url_without_passwd,
       "code": code
     });
   });
-};
+}
 
 function call_callback(http_status, callback) {
   if (callback) {
     callback(http_status);
   }
-};
+}
 
 exports.call_callback = call_callback;
 function test(urls, callback) {
-  var status_set = new Array();
-
-  if (typeof urls == 'string') {
-    urls = [urls]
+  if (typeof urls === 'string') {
+    urls = [urls];
   }
 
   var urls_count = urls.length;
-  for(var i = 0; i < urls_count; i++) {
-    status_test(urls[i], function(http_status) {
-      call_callback(http_status, callback);
-    });
+  function forward_callback(http_status) {
+    call_callback(http_status, callback);
   }
-};
+  for(var i = 0; i < urls_count; i++) {
+    status_test(urls[i], forward_callback);
+  }
+}
 
 function text_output(http_status, callback) {
   if (http_status.error_message) {
     console.error(http_status.url + ' request error: ' + http_status.error_message);
   }
   var final_message = http_status.message;
-  if (http_status.message == 'ERROR') {
+  if (http_status.message === 'ERROR') {
     final_message += '  ';
   }
   console.log(final_message + ' ' + http_status.code + ' ' + http_status.url);
   call_callback(http_status, callback);
-};
+}
 
 exports.test = test
 exports.log = function(url, callback) {
   test(url, function(http_status) { text_output(http_status, callback); });
-};
+}
 
